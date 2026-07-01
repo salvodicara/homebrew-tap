1168ea7172a52560b3e4db3707f73b1d3d0dd2b0
Salvatore Di Cara <salvodicara@gmail.com>
2026-07-01 14:10:09 +0100

    git-recap v0.2.1

--- Formula/git-recap.rb
+++ Formula/git-recap.rb
@@ -1,18 +1,18 @@
 class GitRecap < Formula
   desc "Reconstruct a work journal from your git history"
   homepage "https://github.com/salvodicara/git-recap"
-  url "https://github.com/salvodicara/git-recap/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "e357f72c0b38be0e6b3374e1c1c29b465a10238ca8deaf60a0bcd3b028350876"
+  url "https://github.com/salvodicara/git-recap/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "1201a2ce2ae28969709a65de2912f6ddec2af2978b06549eb09bb29b20698b39"
   license "MIT"
   head "https://github.com/salvodicara/git-recap.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=v#{version}")
   end
 
   test do
-    assert_match "journal_root", shell_output("#{bin}/git-recap config")
+    assert_match version.to_s, shell_output("#{bin}/git-recap version")
   end
 end
