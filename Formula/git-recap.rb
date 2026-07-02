3b9d97cddc99d49474a2444959e04feb5c0aace2
Salvatore Di Cara <salvodicara@gmail.com>
2026-07-02 03:29:00 +0100

    git-recap v0.4.0

--- Formula/git-recap.rb
+++ Formula/git-recap.rb
@@ -1,8 +1,8 @@
 class GitRecap < Formula
   desc "Reconstruct a work journal from your git history"
   homepage "https://github.com/salvodicara/git-recap"
-  url "https://github.com/salvodicara/git-recap/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "4779acd70356c32c3014ecb0d78d08edd0ba23bc8f0cb79cde900b1631fb8331"
+  url "https://github.com/salvodicara/git-recap/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "e0b930c72fd3c10abb3eceb2f8c007d9393791b811b745b06891dc4edfc07b6a"
   license "MIT"
   head "https://github.com/salvodicara/git-recap.git", branch: "main"
 
@@ -10,6 +10,7 @@ class GitRecap < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=v#{version}")
+    generate_completions_from_executable(bin/"git-recap", "completion")
   end
 
   test do
